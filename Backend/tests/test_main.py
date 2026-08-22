96d71d2311ceb75f60e76b42600b72196c47b387
Florencia Balestena <florencia.balestena@estudiantes.utec.edu.uy>
2026-08-22 20:00:22 -0300

    Agregamos pipeline con test de vitest para frontend y pytest para backend

---
+++ Backend/tests/test_main.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "mensaje": "✅ Backend de Interfascia funcionando correctamente"
+    }
+
+
+def test_health():
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "healthy"
+    }
